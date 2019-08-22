8fbd2197c8c37c4939ad82ac7132d82eb0ab8aea
Paul de Nonancourt <pdenonancourt@octo.com>
2019-08-22 22:14:56 +0200

    2 - Convert SourceModels to MongoProduct

--- src/main/java/com/craftingdatascience/bbl/etl/application/Importer.java
+++ src/main/java/com/craftingdatascience/bbl/etl/application/Importer.java
@@ -1,6 +1,8 @@
 package com.craftingdatascience.bbl.etl.application;
 
 import com.craftingdatascience.bbl.etl.domain.FileReader;
+import com.craftingdatascience.bbl.etl.domain.converter.ProductConverter;
+import com.craftingdatascience.bbl.etl.domain.mongo.MongoProduct;
 import com.craftingdatascience.bbl.etl.domain.source.SourceComposition;
 import com.craftingdatascience.bbl.etl.domain.source.SourceProduct;
 import org.springframework.stereotype.Component;
@@ -9,36 +11,59 @@ import org.springframework.util.ResourceUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.groupingBy;
 
 @Component
 public class Importer implements Runnable {
 
     private final FileReader<SourceProduct> sourceProductFileReader;
     private final FileReader<SourceComposition> sourceCompositionFileReader;
+    private final ProductConverter ProductConverter;
 
     public Importer(
             FileReader<SourceProduct> sourceProductFileReader,
-            FileReader<SourceComposition> sourceCompositionFileReader
+            FileReader<SourceComposition> sourceCompositionFileReader,
+            ProductConverter ProductConverter
     ) {
         this.sourceProductFileReader = sourceProductFileReader;
         this.sourceCompositionFileReader = sourceCompositionFileReader;
+        this.ProductConverter = ProductConverter;
     }
 
     public void run() {
         try {
             System.out.println("Retrieving data from files...");
-            // This could be read directly from a blob storage
             InputStream productsInputStream = Files.newInputStream(ResourceUtils.getFile("classpath:models.csv").toPath());
             InputStream compositionsInputStream = Files.newInputStream(ResourceUtils.getFile("classpath:compositions.csv").toPath());
 
-            System.out.println("Parsing data and mapping to POJOs...");
-            sourceCompositionFileReader.read(compositionsInputStream)
-                .forEach(System.out::println);
+            System.out.println("Parsing data from compositions to POJOs...");
+            Map<String, List<SourceComposition>> compositionsByProductCode = groupCompositionsByProductCode(compositionsInputStream);
+
+            System.out.println("Parsing and converting product models...");
             sourceProductFileReader.read(productsInputStream)
-                .forEach(System.out::println);
+                    .map(product -> convert(compositionsByProductCode, product))
+                    .forEach(System.out::println);
         } catch (IOException e) {
             System.out.println("Something went wrong here...");
         }
         System.out.println("Job's done!");
+    }
+
+    private Map<String, List<SourceComposition>> groupCompositionsByProductCode(InputStream compositionsInputStream) throws IOException {
+        Stream<SourceComposition> sourceCompositionStream = sourceCompositionFileReader.read(compositionsInputStream);
+        return sourceCompositionStream.collect(groupingBy(SourceComposition::getProductCode));
+    }
+
+    private MongoProduct convert(Map<String, List<SourceComposition>> compositionsByProductCode, SourceProduct Product) {
+        List<SourceComposition> compositions = compositionsByProductCode.getOrDefault(
+                Product.getProductCode(),
+                emptyList()
+        );
+        return ProductConverter.adapt(Product, compositions);
     }
 }
